56b626d297b911b82b7d77f927a476f387a7b82e
Anderson Meira <andersonmeira@outlook.com>
2017-12-18 21:58:06 -0200

    creating podcast model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Podcast(models.Model):
+    name = models.CharField("Podcast name", max_length=128)
+    feed = models.URLField("Podcast feed", unique=True)
+    image = models.CharField("Podcast thumbnail", max_length=200, \
+                                default="/static/dist/img/podcast-banner.png")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    last_feed_update = models.DateTimeField( default=timezone.now)
+
+    class Meta:
+        db_table = "podcast"
+        verbose_name = "Podcast"
+        verbose_name_plural = "Podcasts"
+
+    def __str__(self):
+        return self.name
+
+
--- podigger/settings.py
+++ podigger/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'web.apps.WebConfig',
+    'core.apps.CoreConfig',
 ]
 
 MIDDLEWARE = [
@@ -103,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
