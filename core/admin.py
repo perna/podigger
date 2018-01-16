b6711e205f5ccb323bdfb66e9561e258885e6d07
Anderson Meira <anderson.meira@gmail.com>
2018-01-16 20:17:36 -0200

    restore backup

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Podcast, Episode
 
-# Register your models here.
+class PodcastAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Podcast, PodcastAdmin)
