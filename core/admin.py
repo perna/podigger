775d5bde79968a036273facc77455917482e3030
Anderson Meira <anderson.meira@gmail.com>
2018-07-19 15:39:10 -0300

    enable Podcast and Episode models on Django Admin

--- core/admin.py
+++ core/admin.py
@@ -4,4 +4,8 @@ from .models import Podcast, Episode
 class PodcastAdmin(admin.ModelAdmin):
     pass
 
+class EpisodeAdmin(admin.ModelAdmin):
+    pass
+
 admin.site.register(Podcast, PodcastAdmin)
+admin.site.register(Episode, EpisodeAdmin)
