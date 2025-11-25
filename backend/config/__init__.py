751ed400a98765077e0077735bfb032127d39888
Anderson Meira <anderson.meira@gmail.com>
2025-11-25 12:45:28 -0300

    refactor: clean config __init__

--- backend/config/__init__.py
+++ backend/config/__init__.py
@@ -1,4 +1,5 @@
 # config package for Django project
 from .celery import app as celery_app
+from .__version__ import __version__
 
-__all__ = ('celery_app',)
+__all__ = ('celery_app', '__version__')
