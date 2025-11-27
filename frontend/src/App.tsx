98cd31059cd850beee07b56b51e5da06bf8b2144
Anderson Meira <anderson.meira@gmail.com>
2025-11-27 16:54:08 -0300

    refactor: removing boilerplate files from frontend project

--- frontend/src/App.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function App() {
-  return (
-    <div style={{ padding: 20 }}>
-      <h1>Podigger Frontend (Vite + React + TypeScript)</h1>
-      <p>Consome a API Django no backend.</p>
-    </div>
-  )
-}
