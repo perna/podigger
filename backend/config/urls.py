1f7fc528792542617636f7b12bb87f3087d25269
Anderson Meira <anderson.meira@gmail.com>
2026-02-21 15:13:59 -0300

    fix: health checks

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from podcasts.health import health_check
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("podcasts.urls")),
+    path("health/", health_check, name="health_check"),
 ]
